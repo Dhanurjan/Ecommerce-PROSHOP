4da463968b5c0b8bced6560fb185e22dfb03a3e6
Dhanurjan Suthakaran <dhanurjan98@gmail.com>
2020-12-30 22:37:16 +0530

    Product Details Screen

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -5,28 +5,30 @@ import Rating from './Rating'
 
 const Product = ({ product }) => {
   return (
-    <Card className='my-3 p-3 rounded'>
-      <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant='top' />
-      </Link>
-      <Card.Body>
+    <>
+      <Card className='my-3 p-3 rounded'>
         <Link to={`/product/${product._id}`}>
-          <Card.Title as='div'>
-            <strong>{product.name}</strong>
-          </Card.Title>
+          <Card.Img src={product.image} variant='top' />
         </Link>
-        <Card.Text as='div'>
-          {/* <div className='my-3'>
+        <Card.Body>
+          <Link to={`/product/${product._id}`}>
+            <Card.Title as='div'>
+              <strong>{product.name}</strong>
+            </Card.Title>
+          </Link>
+          <Card.Text as='div'>
+            {/* <div className='my-3'>
             {product.rating} from {product.numReviews} reviews
           </div> */}
-          <Rating
-            value={product.rating}
-            text={`${product.numReviews}reviews`}
-          />
-        </Card.Text>
-        <Card.Text as='h3'>${product.price}</Card.Text>
-      </Card.Body>
-    </Card>
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews}reviews`}
+            />
+          </Card.Text>
+          <Card.Text as='h3'>${product.price}</Card.Text>
+        </Card.Body>
+      </Card>
+    </>
   )
 }
 

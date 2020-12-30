5abe5c51dadee8ca768a49eadbd8640fa36e19e7
Dhanurjan Suthakaran <dhanurjan98@gmail.com>
2020-12-30 11:39:06 +0530

    Rating Component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -9,7 +9,7 @@ const HomeScreen = () => {
       <h1>Latest products</h1>
       <Row>
         {products.map((product) => (
-          <Col sm={12} md={6} lg={3}>
+          <Col key={product._id} sm={12} md={6} lg={3}>
             {/* <h3>{product.name}</h3> */}
             <Product product={product} />
           </Col>
